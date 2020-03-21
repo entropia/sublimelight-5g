46cd84e1d6ac14adead8bde2bdb735bcc236fd79
Andreas Fried <andi@0xaf.de>
2020-03-21 13:02:19 +0100

    Use statically allocated light_manager_state_t.
    Right now there is only ever one state object, so we have to need to
    allocate it on the heap.

--- firmware/main/light_manager.c
+++ firmware/main/light_manager.c
@@ -4,32 +4,29 @@
 
 ESP_EVENT_DEFINE_BASE(LIGHT_MANAGER_EVENT);
 
-light_manager_state_t *state;
+light_manager_state_t state;
 
 void light_manager_init()
 {
-	light_manager_state_t *new_state = malloc(sizeof(light_manager_state_t));
-	assert(new_state);
-	new_state->warm_value = 0;
-	new_state->cold_value = 0;
-	state = new_state;
+	state.warm_value = 0;
+	state.cold_value = 0;
 }
 
 void set_warm(unsigned intensity)
 {
 	led_set(WARM_WHITE, intensity);
-	state->warm_value = intensity;
+	state.warm_value = intensity;
 	update_state(LIGHT_MANAGER_EVENT_WARM_CHANGED);
 }
 
 void set_cold(unsigned intensity)
 {
 	led_set(COLD_WHITE, intensity);
-	state->cold_value = intensity;
+	state.cold_value = intensity;
 	update_state(LIGHT_MANAGER_EVENT_COLD_CHANGED);
 }
 
 void update_state(light_manager_event_t event)
 {
-	esp_event_post(LIGHT_MANAGER_EVENT, event, state, sizeof(light_manager_state_t), portMAX_DELAY);
+	esp_event_post(LIGHT_MANAGER_EVENT, event, &state, sizeof(light_manager_state_t), portMAX_DELAY);
 }
