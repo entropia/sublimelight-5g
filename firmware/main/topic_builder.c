6be682f2107545622c24b74132c62876e97cb6e8
Andreas Fried <andi@0xaf.de>
2020-05-24 01:52:18 +0200

    Implement room topics.

--- firmware/main/topic_builder.c
+++ firmware/main/topic_builder.c
@@ -8,7 +8,8 @@
 
 static const char *TAG = "SL5G_TOPIC_BUILDER";
 
-static const char *CMND_TOPIC_PREFIX = "cmnd/sl5g/id";
+static const char *CMND_TOPIC_PREFIX_ID = "cmnd/sl5g/id";
+static const char *CMND_TOPIC_PREFIX_ROOM = "cmnd/sl5g/room";
 static const char *STAT_TOPIC_PREFIX = "stat/sl5g/id";
 
 static const char *CMND_TOPIC_SUFFIXES[CMND_END] =
@@ -44,7 +45,9 @@ typedef struct
 	char *topic;
 } stat_map_entry_t;
 
-cmnd_map_entry_t cmnd_map[CMND_END];
+#define CMND_MAP_ENTRIES (2 * CMND_END)
+
+cmnd_map_entry_t cmnd_map[CMND_MAP_ENTRIES];
 stat_map_entry_t stat_map[STAT_END];
 
 char* stat_topic_lookup(stat_event_t event)
@@ -54,7 +57,7 @@ char* stat_topic_lookup(stat_event_t event)
 
 cmnd_event_t cmnd_topic_lookup(char *topic)
 {
-	for (int i = 0; i < CMND_END; i++)
+	for (int i = 0; i < CMND_MAP_ENTRIES; i++)
 	{
 		if(!strcmp(topic, cmnd_map[i].topic)) {
 			return cmnd_map[i].event;
@@ -65,7 +68,7 @@ cmnd_event_t cmnd_topic_lookup(char *topic)
 
 void subscribe_to_initial_topics()
 {
-	for (int i = 0; i < CMND_END; i++) {
+	for (int i = 0; i < CMND_MAP_ENTRIES; i++) {
 		mqtt_client_subscribe(cmnd_map[i].topic);
 	}
 }
@@ -73,8 +76,7 @@ void subscribe_to_initial_topics()
 static void build_topics()
 {
 	nvs_config_t *config = nvs_config_get();
-	for (int i = 0; i < STAT_END; i++)
-	{
+	for (int i = 0; i < STAT_END; i++) {
 		//stat_map_entry_t *entry = malloc(sizeof(stat_map_entry_t));
 		//stat_map[i] = *entry;
 		//entry->event = i;
@@ -82,11 +84,16 @@ static void build_topics()
 		asprintf(&stat_map[i].topic, "%s/%s/%s", STAT_TOPIC_PREFIX, config->device_id, STAT_TOPIC_SUFFIXES[i]);
 	}
 
-	for (int i = 0; i < CMND_END; i++)
-	{
-		cmnd_map[i].event = i;
-		asprintf(&cmnd_map[i].topic, "%s/%s/%s", CMND_TOPIC_PREFIX, config->device_id, CMND_TOPIC_SUFFIXES[i]);
+	int i = 0;
+	for (int cmnd = 0; cmnd < CMND_END; cmnd++, i++) {
+		cmnd_map[i].event = cmnd;
+		asprintf(&cmnd_map[i].topic, "%s/%s/%s", CMND_TOPIC_PREFIX_ID, config->device_id, CMND_TOPIC_SUFFIXES[cmnd]);
 	}
+	for (int cmnd = 0; cmnd < CMND_END; cmnd++, i++) {
+		cmnd_map[i].event = cmnd;
+		asprintf(&cmnd_map[i].topic, "%s/%s/%s", CMND_TOPIC_PREFIX_ROOM, config->room_name, CMND_TOPIC_SUFFIXES[cmnd]);
+	}
+	assert(i == CMND_MAP_ENTRIES);
 }
 
 static void on_nvs_config_changed(void *arg, esp_event_base_t base, int32_t event_id, void *event_data)
