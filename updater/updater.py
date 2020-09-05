66e6dda2adc675f3d0b2955594a6159794277020
Andreas Fried <andi@0xaf.de>
2020-09-06 01:09:18 +0200

    Make updater nicer with curses, and implement proper shutdown.

--- updater/updater.py
+++ updater/updater.py
@@ -2,9 +2,9 @@
 
 import argparse
 import collections
+import curses
 import dataclasses
 import enum
-import functools
 import http
 import http.client
 import http.server
@@ -13,7 +13,7 @@ import queue
 import re
 import shutil
 import socket
-import sys
+import time
 import threading
 import urllib
 
@@ -32,16 +32,16 @@ def get_my_ip(test_host):
     test_host. If that address is a loopback address tries again using
     1.1.1.1 as test_host."""
 
-    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-        s.connect((test_host, 1))
-        result = s.getsockname()[0]
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+        sock.connect((test_host, 1))
+        result = sock.getsockname()[0]
 
     if not ipaddress.ip_address(result).is_loopback:
         return result
 
-    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-        s.connect(("1.1.1.1", 1))
-        return s.getsockname()[0]
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+        sock.connect(("1.1.1.1", 1))
+        return sock.getsockname()[0]
 
 
 def start_panel_update(panel, event_queue):
@@ -65,42 +65,67 @@ class EventQueue():
     FoundIP = collections.namedtuple("FoundIp", "panel_id ip")
     UpdateRequested = collections.namedtuple("UpdateRequested", "panel_id")
     DownloadStarted = collections.namedtuple("DownloadStarted", "ip")
+    RegisterShutdownHook = collections.namedtuple("RegisterShutdownHook", "hook")
     Shutdown = collections.namedtuple("Shutdown", "")
 
     Panel = dataclasses.make_dataclass("Panel", ["panel_id", "ip", "state"])
-    PanelState = enum.Enum("IPState", "NEW UPDATE_REQUESTED DOWNLOAD_STARTED REBOOTED")
+    PanelState = enum.Enum("IPState", "NEW UPDATE_REQUESTED DOWNLOADING REBOOTED")
 
+    def __init__(self, curses_window):
+        curses_window.nodelay(True)
 
-    def __init__(self):
-        self.queue = queue.Queue(maxsize=1)
+        self.curses_window = curses_window
+        self.queue = queue.Queue()
         self.panels = []
+        self.shutdown_hooks = []
+        self.messages = []
 
 
-    def start(self):
-        def runner():
+    def run(self):
+        try:
             while True:
-                msg = self.queue.get()
-                if type(msg) == self.Shutdown:
-                    return
-                elif type(msg) == self.FoundIP:
+                in_char = self.curses_window.getch()
+                if in_char == 10:
+                    break
+
+                try:
+                    msg = self.queue.get(block=False)
+                except queue.Empty:
+                    continue
+
+                if isinstance(msg, self.Shutdown):
+                    break
+                elif isinstance(msg, self.FoundIP):
                     self.handle_found_ip(msg)
-                elif type(msg) == self.UpdateRequested:
+                elif isinstance(msg, self.UpdateRequested):
                     self.handle_update_requested(msg)
-                elif type(msg) == self.DownloadStarted:
+                elif isinstance(msg, self.DownloadStarted):
                     self.handle_download_started(msg)
+                elif isinstance(msg, self.RegisterShutdownHook):
+                    self.handle_register_shutdown_hook(msg)
 
                 self.print_panels()
-
-        queue_thread = threading.Thread(target=runner, name="Event queue thread")
-        queue_thread.start()
+        finally:
+            for hook in self.shutdown_hooks:
+                hook()
 
 
     def print_panels(self):
-        print("| id | IP address      | state            |")
-        print("+----+-----------------+------------------+")
+        result = ""
+        result += "| id | IP address      | state            |\n"
+        result += "+----+-----------------+------------------+\n"
         for p in self.panels:
-            print("| {:<2} | {:<15} | {:<16} |".format(p.panel_id, p.ip, p.state.name))
-        print("")
+            result += "| {:<2} | {:<15} | {:<16} |\n".format(p.panel_id, p.ip, p.state.name)
+
+        result += "\nPress Enter to exit. Warning: Do not exit when panels are DOWNLOADING\n\n"
+
+        for m in self.messages:
+            result += m + "\n"
+
+        self.curses_window.clear()
+        self.curses_window.addstr(0, 0, result)
+        self.curses_window.refresh()
+
 
     def handle_found_ip(self, msg):
         panel_id = msg.panel_id
@@ -110,18 +135,19 @@ class EventQueue():
             # We have seen this panel before, assume it has rebooted and is therefore done.
             # However, remember that it may have got another IP address this time.
             panel = match[0]
-            if panel.state in [self.PanelState.DOWNLOAD_STARTED, self.PanelState.REBOOTED]:
+            if panel.state in [self.PanelState.DOWNLOADING, self.PanelState.REBOOTED]:
                 panel.ip = msg.ip
                 panel.state = self.PanelState.REBOOTED
             else:
-                print("!!! Panel {}: IP changed ({} -> {}) before reboot was due".format(panel_id, panel.ip, msg.ip))
-                states[panel_id] = prev_state._update(ip=msg.ip)
+                self.messages.append("!!! Panel {}: IP changed ({} -> {}) before reboot was due".format(panel_id, panel.ip, msg.ip))
+                self.panels[panel_id] = panel._update(ip=msg.ip)
 
         else:
             # This is a new panel. Make a note of it and request it to update
             new_panel = self.Panel(panel_id, msg.ip, self.PanelState.NEW)
             self.panels.append(new_panel)
-            start_panel_update(new_panel, self)
+            self.messages.append("Update disabled")
+            # art_panel_update(new_panel, self)
 
 
     def handle_update_requested(self, msg):
@@ -129,17 +155,17 @@ class EventQueue():
         match = [p for p in self.panels if p.panel_id == msg.panel_id]
 
         if match == []:
-            print("!!! Panel {}: Not yet known but update request already sent".format(panel_id))
+            self.messages.append("!!! Panel {}: Not yet known but update request already sent".format(panel_id))
 
         else:
             panel = match[0]
             if panel.state == self.PanelState.NEW:
                 panel.state = self.PanelState.UPDATE_REQUESTED
-            elif panel.state == self.PanelState.DOWNLOAD_STARTED:
+            elif panel.state == self.PanelState.DOWNLOADING:
                 # This is not a problem, the DownloadStarted message has just overtaken us
                 pass
             else:
-                print("!!! Panel {}: Spurious request sent".format(panel_id))
+                self.messages.append("!!! Panel {}: Spurious request sent".format(panel_id))
 
 
     def handle_download_started(self, msg):
@@ -147,16 +173,20 @@ class EventQueue():
         match = [p for p in self.panels if p.ip == msg.ip]
 
         if match == []:
-            print("!!! Unknown download from {}".format(ip))
+            self.messages.append("!!! Unknown download from {}".format(ip))
 
         else:
             panel = match[0]
             if panel.state in [self.PanelState.NEW, self.PanelState.UPDATE_REQUESTED]:
                 # If we see NEW, we have overtaken the UpdateRequested message.
                 # This is not a problem.
-                panel.state = self.PanelState.DOWNLOAD_STARTED
+                panel.state = self.PanelState.DOWNLOADING
             else:
-                print("!!! Panel {}: Spurious download".format(panel.id))
+                self.messages.append("!!! Panel {}: Spurious download".format(panel.id))
+
+
+    def handle_register_shutdown_hook(self, msg):
+        self.shutdown_hooks.append(msg.hook)
 
 
     def post_found_ip(self, panel_id, ip):
@@ -171,15 +201,12 @@ class EventQueue():
         self.queue.put(self.DownloadStarted(ip))
 
 
+    def post_register_shutdown_hook(self, hook):
+        self.queue.put(self.RegisterShutdownHook(hook))
+
+
     def post_shutdown(self):
         self.queue.put(self.Shutdown())
-
-
-def start_event_queue():
-    queue = EventQueue()
-    queue.start()
-
-    return queue
 
 
 def start_binary_server(event_queue, binary_path):
@@ -206,12 +233,19 @@ def start_binary_server(event_queue, binary_path):
 
             shutil.copyfileobj(binary_file, self.wfile)
 
-
-    httpd = http.server.ThreadingHTTPServer((HTTP_LISTEN_HOST, HTTP_PORT), HTTPHandler)
+    try:
+        httpd = http.server.ThreadingHTTPServer((HTTP_LISTEN_HOST, HTTP_PORT), HTTPHandler)
+    except OSError as e:
+        print("Could not open HTTP server: {}".format(e))
+        event_queue.post_shutdown()
 
     def serve():
         httpd.serve_forever()
-        print("HTTP server has shut down")
+
+    def shutdown():
+        httpd.shutdown()
+
+    event_queue.post_register_shutdown_hook(shutdown)
 
     httpd_thread = threading.Thread(target=serve, name="HTTP server thread")
     httpd_thread.start()
@@ -221,10 +255,10 @@ def start_binary_server(event_queue, binary_path):
 
 def start_mqtt_listener(event_queue, mqtt_server_host, mqtt_port):
 
-    def on_mqtt_connect(client, userdata, flags, rc):
+    def on_mqtt_connect(client, _userdata, _flags, _rc):
         client.subscribe(SL5G_IP_TOPIC)
 
-    def on_mqtt_publish(client, userdata, msg):
+    def on_mqtt_publish(_client, _userdata, msg):
         panel_id = re.match(SL5G_PANEL_REGEX, msg.topic).group(1)
         event_queue.post_found_ip(panel_id, msg.payload.decode("utf-8"))
 
@@ -233,13 +267,23 @@ def start_mqtt_listener(event_queue, mqtt_server_host, mqtt_port):
     client.on_connect = on_mqtt_connect
     client.on_message = on_mqtt_publish
 
-    client.connect(mqtt_server_host, mqtt_port, 60)
+    try:
+        client.connect(mqtt_server_host, mqtt_port, 60)
+    except OSError as e:
+        print("Could not connect to MQTT server: {}".format(e))
+        event_queue.post_shutdown()
+
+    def shutdown():
+        client.loop_stop()
+
+    event_queue.post_register_shutdown_hook(shutdown)
+
     client.loop_start()
 
     return client
 
 
-def main():
+def main(stdscr):
     parser = argparse.ArgumentParser(description="Updater for sublimelight-5")
     parser.add_argument("binary", metavar="BINARY",
                         help="Path to the new firmware binary")
@@ -251,18 +295,14 @@ def main():
                         help="Listening port of the MQTT server where the light modules publish their status")
     args = parser.parse_args()
 
-    event_queue = start_event_queue()
+    stdscr.nodelay(True)
+
+    event_queue = EventQueue(stdscr)
     binary_server = start_binary_server(event_queue, args.binary)
     mqtt_listener = start_mqtt_listener(event_queue, args.mqtt_server, args.mqtt_port)
 
-    input()
-
-    event_queue.post_shutdown()
-    binary_server.shutdown()
-    mqtt_listener.loop_stop()
-
-    return 0
+    event_queue.run()
 
 
 if __name__ == "__main__":
-    sys.exit(main())
+    curses.wrapper(main)
